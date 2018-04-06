01a1d81dda25cfb78dd9c5748f149c9ce7adecda
Jakob <jwg2@illinois.edu>
2018-04-05 21:37:20 -0500

    Reorganized code, added gifs, examples, and test ipynb

--- dodo.py
+++ dodo.py
@@ -1,10 +1,14 @@
 from doit.tools import run_once
 import h5py
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.animation
+from nbody_code.ParticleClasses import *
+from mpl_toolkits.mplot3d import Axes3D
 
 def task_generate_gaussian():
-    N = 32**3
+    N = 32**2
     seed = 0x4d3d3d3
     fn = "gaussian.h5"
     def _generate():
@@ -18,3 +22,70 @@ def task_generate_gaussian():
     return {'actions': [_generate],
             'targets': [fn],
             'uptodate': [run_once]}
+
+def task_user_simulation():
+
+    input = "gaussian.h5"
+    output = "gaussian_after.h5"
+
+    def _simulation():
+        ###################
+        #NAME OF HDF5 FILE#
+        ###################
+
+        data_file = h5py.File(input, 'r')
+
+        #########################
+        #BACKGROUND FIELD AND   #
+        #TOGGLE ONLY BACKGROUND #
+        #########################
+        x = 0
+        y = 0
+        z = 0
+        background = [x,y,z]
+
+        strictly_background = False
+
+        ##########################
+        #INTEGRATOR SELECTION    #
+        #0 = direct euler        #
+        #1 = implicit euler      #
+        #2 = leapfrog            #
+        ##########################
+
+        integrator = 0
+
+        # List all groups
+        print("Keys: %s" % list(data_file.keys()))
+        a_group_key = list(data_file.keys())[0]
+
+        # Get the data
+        positions = list(data_file['/particle_positions'])
+        velocities = list(data_file['/particle_velocities'])
+        masses = list(data_file['/particle_masses'])
+
+        particles = []
+        #Build particles from HDF5 data
+        for i in range (len(positions)):
+            particle = Particle(positions[i][0], positions[i][1], positions[i][2],
+                                velocities[i][0], velocities[i][1], velocities[i][2],
+                                masses[i])
+            particles.append(particle)
+
+        engine = Engine(background, particles, strictly_background)
+        engine.euler(0, integrator)
+
+        runtime = 1.0
+        step = .5
+
+        while (runtime > engine.t):
+            engine.euler(step, integrator)
+
+        engine.write_to_file(output)
+        data_file.close()
+
+    return {
+	'actions': [_simulation],
+    'targets': [output],
+    'uptodate': [None]
+    }
